356bf78d378625a6646a1976adbda2ae018b5114
Ezi-umeonu Menuchim <msezi.usv@gmail.com>
2026-08-17 16:08:24 +0100

    updated skills section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,27 @@
 import { getMyDetails } from "@/sanity/query";
 import { BiLogoTypescript } from "react-icons/bi";
-import { FaReact, FaGitAlt } from "react-icons/fa";
+import { FaReact, FaGitAlt, FaNode, FaSass } from "react-icons/fa";
 import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
 import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
-import { SiAdobephotoshop, SiRedux, SiSanity } from "react-icons/si";
+import { SiAdobephotoshop, SiExpress, SiPrisma, SiRedux, SiSanity, SiSupabase } from "react-icons/si";
 
 export default async function Home() {
   const stack = [
     { icon: <IoLogoJavascript size={20} />, title: "Javascript" },
-    { icon: <FaGitAlt size={20} />, title: "Git" },
-    { icon: <FaReact size={20} />, title: "React Js" },
     { icon: <BiLogoTypescript size={20} />, title: "Typescript" },
-    { icon: <IoLogoFirebase size={20} />, title: "Firebase" },
+    { icon: <FaReact size={20} />, title: "React Js" },
     { icon: <RiNextjsLine size={20} />, title: "Next Js" },
-    { icon: <SiSanity size={20} />, title: "Sanity CMS" },
+    { icon: <FaNode size={20} />, title: "Node.js" },
+    { icon: <SiExpress size={20} />, title: "Express" },
+    { icon: <SiPrisma size={20} />, title: "Prisma" },
+    { icon: <SiSupabase size={20} />, title: "Supabase" },
     { icon: <RiTailwindCssFill size={20} />, title: "Tailwind CSS" },
+    { icon: <FaSass size={20} />, title: "Sass" },
+    { icon: <SiRedux size={20} />, title: "Redux Toolkit" },
+    { icon: <IoLogoFirebase size={20} />, title: "Firebase" },
+    { icon: <SiSanity size={20} />, title: "Sanity CMS" },
+    { icon: <FaGitAlt size={20} />, title: "Git" },
     { icon: <SiAdobephotoshop size={20} />, title: "Adobe Photoshop" },
-    { icon: <SiRedux size={20} />, title: "Redux toolkit" },
   ];
 
   const data = await getMyDetails();
